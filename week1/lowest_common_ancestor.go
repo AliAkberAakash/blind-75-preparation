8be40751079d48471a8fa1a545d8c7041fcdd6a3
Ali Akber <ali852609@gmail.com>
2022-05-06 22:41:30 +0600

    Create lowest_common_ancestor.go

---
+++ week1/lowest_common_ancestor.go
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val   int
+ *     Left  *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+func lowestCommonAncestor(root, p, q *TreeNode) *TreeNode {
+    if p.Val == root.Val || q.Val == root.Val {
+        return root
+    }
+    
+    if p.Val > root.Val && q.Val < root.Val {
+        return root
+    } else if p.Val < root.Val && q.Val > root.Val {
+        return root
+    }
+    
+    if p.Val > root.Val && q.Val > root.Val {
+        return lowestCommonAncestor(root.Right,p,q)
+    }
+    
+    if p.Val < root.Val && q.Val < root.Val {
+        return lowestCommonAncestor(root.Left,p,q)
+    }
+    
+    return root
+}
